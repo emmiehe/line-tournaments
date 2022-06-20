3972f843beaa60c46e17eec321fe3a92499c3c16
Emmie He <yimin_he@brown.edu>
2022-06-20 14:09:12 -0700

    build the board and added parse_position

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,116 @@
+use std::error::Error;
+use std::io;
+use std::num::ParseIntError;
+
+#[derive(Clone, PartialOrd, PartialEq, Debug)]
+enum Intersection {
+    Empty,
+    Player(usize),
+}
+
+type Board = Vec<Vec<Intersection>>;
+
+fn make_board(n: usize) -> Board {
+    vec![vec![Intersection::Empty; n]; n]
+}
+
+#[derive(PartialOrd, PartialEq, Debug)]
+struct Position(usize, usize); // (row, col)
+
+fn parse_position_part(s: &str, board_size: usize) -> Result<usize, String> {
+    let pos = s.trim().parse::<usize>().map_err(|e| e.to_string())?;
+    if pos >= board_size {
+        Err(String::from("Too large"))
+    } else {
+        Ok(pos)
+    }
+
+    // match s.trim().parse::<usize>() {
+    //     Ok(pos) => {
+    //         if pos >= board_size {
+    //             Err(String::from("Too large"))
+    //         } else {
+    //             Ok(pos)
+    //         }
+    //     }
+    //     Err(e) => Err(e.to_string()),
+    // }
+
+    // if let Ok(pos) = s.trim().parse::<usize>() {
+    //     // process pos
+    //     if pos >= board_size {
+    //         Err(String::from("Too large"))
+    //     } else {
+    //         Ok(pos)
+    //     }
+    // } else {
+    //     Err(String::from("parse position part err"))
+    // }
+}
+
+fn parse_position(s: &str, board_size: usize) -> Result<Position, String> {
+    let parts = s.trim().split_whitespace().collect::<Vec<&str>>();
+
+    if parts.len() != 2 {
+        return Err(String::from("Not 2 parts."));
+    }
+
+    Ok(Position(parse_position_part(parts[0], board_size)?, parse_position_part(parts[1], board_size)?))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let num_players = 2;
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut input_position = String::new();
+
+        io::stdin()
+            .read_line(&mut input_position)
+            .expect("Failed to read line");
+
+        // let guess: u32 = match guess.trim().parse() {
+        //     Ok(num) => num,
+        //     Err(_) => continue,
+        // };
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_board() {
+        let n = 15;
+        let board = make_board(n);
+        assert_eq!(n, board.len());
+        for row in board {
+            assert_eq!(n, row.len());
+            for intersection in row {
+                assert_eq!(Intersection::Empty, intersection);
+            }
+        }
+    }
+
+    #[test]
+    fn test_parse_position() {
+        let board_size = 15;
+        let s = "12 14";
+        assert_eq!(Position(12, 14), parse_position(s, board_size).unwrap());
+
+        let s = "0 1";
+        assert_eq!(Position(0, 1), parse_position(s, board_size).unwrap());
+
+        let s = " 12  14  ";
+        assert_eq!(Position(12, 14), parse_position(s, board_size).unwrap());
+
+        let bad_strings = ["12 15", "-1 2", "12.0 15", "12 1 1", "he he", "1, 2", "", "\n"];
+
+        for s in bad_strings {
+            assert!(parse_position(s, board_size).is_err());
+        }
+    }
 }
